c3bec881c625970a45680f51cc1006c35b389fee
Ritul Jain <rituljain0708@gmail.com>
2022-11-24 19:12:01 +0530

    Sign in form created, sign up form is next

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -3,7 +3,19 @@ import './SignUpPage.css'
 
 const SignUpPage = () => {
   return (
-    <div>SignUpPage</div>
+    <div className='signupPage'>
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder='Email' />
+        <input type="password" placeholder='Password' />
+        <button type='submit'>Sign In</button>
+
+        <h4>
+          <span className="signupPage-gray">New to Netflix?</span>
+          <span className="signupPage-link">Sign Up Now</span>
+        </h4>
+      </form>
+    </div>
   )
 }
 
